706e7cc5e3c42dbd1244208d2de79c8deb48f4df
Gerald161 <56877559+Gerald161@users.noreply.github.com>
2025-06-19 07:19:04 +0000

    Add files via upload

---
+++ js/sidebarcontroller.js
@@ -0,0 +1,36 @@
+var sideBarButton = document.querySelector(".nav_icon");
+
+var closeButton = document.querySelector(".close_button");
+
+var sideBar = document.querySelector(".mobile_screen_sidebar");
+
+var drawerOverlay = document.querySelector(".drawer_overlay");
+
+sideBarButton.addEventListener("click", (e)=>{
+    sideBar.style.left = "0";
+    drawerOverlay.style.opacity = 1;
+    drawerOverlay.style.pointerEvents = "auto";
+});
+
+closeButton.addEventListener("click", (e)=>{
+    sideBar.style.left = "-30vw";
+    drawerOverlay.style.opacity = 0;
+    drawerOverlay.style.pointerEvents = "none";
+});
+
+
+drawerOverlay.addEventListener("click", (e)=>{
+    sideBar.style.left = "-30vw";
+    drawerOverlay.style.opacity = 0;
+    drawerOverlay.style.pointerEvents = "none";
+})
+
+window.onresize = checkWindowSize;
+
+function checkWindowSize(){
+    if(window.innerWidth <= '700'){
+        console.log("small")
+    }else{
+        console.log("large")
+    }
+}
